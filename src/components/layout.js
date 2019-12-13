a5683eb68ed3a14e78331478ce628fa9a0f7a4dc
Tiffani <tiffani.grace.elmore@gmail.com>
2019-12-12 19:26:38 -0600

    remove lodash from blog index

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import base from './base.css'
 import Container from './container'
 import Navigation from './navigation'
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,39 +1,37 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import styles from './blog.module.css'
 import Layout from "../components/layout"
 import ArticlePreview from '../components/article-preview'
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+const BlogIndex = () => {
+  const posts = data.allContentfulBlogPost.edges
 
-    return (
-      <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <div className={styles.hero}>
-            <h1 className={styles.heroHeadline}>Blog</h1>
-          </div>
-          <div className="wrapper">
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+  return (
+    <Layout>
+      <div style={{ background: '#fff' }}>
+        <Helmet title={siteTitle} />
+        <div className={styles.hero}>
+          <h1 className={styles.heroHeadline}>Blog</h1>
         </div>
-      </Layout>
-    )
-  }
+        <div className="wrapper">
+          <ul className="article-list">
+            {posts.map(({ node }) => {
+              return (
+                <li key={node.slug}>
+                  <ArticlePreview article={node} />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  )
 }
+    
+
 
 export default BlogIndex
 
