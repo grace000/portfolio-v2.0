36889b9768e59312e01401fb12b25f328f5958fa
Tiffani <tiffani.grace.elmore@gmail.com>
2019-12-31 15:04:10 -0600

    decrease navigation link margin

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,16 +4,16 @@ import { Link } from 'gatsby'
 export default () => (
   <nav role="navigation">
     <ul className="navigation">
-      <li className="navigationItem">
+      <li className="navigation-item">
         <Link to="/">Home</Link>
       </li>
-      <li className="navigationItem">
+      <li className="navigation-item">
         <Link to="/blog">Blog</Link>
       </li>
-      <li className="navigationItem">
+      <li className="navigation-item">
         <Link to="/art">Art</Link>
       </li>
-      <li className="navigationItem">
+      <li className="navigation-item">
         <Link to="/me">Me</Link>
       </li>
     </ul>
